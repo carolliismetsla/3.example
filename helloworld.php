731bd5d48044ea67d1c63bd27f88c5dbd83c9428
carmet <carmet@greeny.cs.tlu.ee>
2016-02-11 12:42:50 +0200

    added first lines of php

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,14 @@
-Hello
+<?php
+
+//Variable starts with $,
+// string has to use qoutation marks
+//line ends with semicolon
+$first_name = "Carol";
+$last_name = "Metsla";
+
+//show code to browser
+//For adding up numbers we use + sign
+//for adding up strings we use . sign
+echo $first_name." ".$last_name;
+
+ ?>
