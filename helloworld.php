0777159d5898ce22bbbd002b9f6fb0f968e32ed7
carmet <carmet@greeny.cs.tlu.ee>
2016-02-11 12:55:27 +0200

    added if statement

--- helloworld.php
+++ helloworld.php
@@ -11,4 +11,27 @@ $last_name = "Metsla";
 //for adding up strings we use . sign
 echo $first_name." ".$last_name;
 
- ?>
+// >greater than
+// <smaller than
+// > = greater than or equal
+// < = smaller than or equal
+// == equal
+// != not equal
+
+$age = 5;
+
+//if age is smaller than or equal to 6, then you can sleep at lunchtime
+if($age <= 6){
+
+
+
+
+  //it is true
+  echo " yes you can sleep at lunchtime";
+
+}else{
+  //it is false
+  echo " no, you are at school";
+  }
+
+   ?>
